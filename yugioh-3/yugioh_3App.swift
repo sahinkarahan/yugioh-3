ad22c24e52342b5a52bc8cb921d527acaf48bf19
sahinkarahan <sahin.karahan@hotmail.com>
2024-12-13 14:52:29 +0300

    Initial Commit

---
+++ yugioh-3/yugioh_3App.swift
@@ -0,0 +1,17 @@
+//
+//  yugioh_3App.swift
+//  yugioh-3
+//
+//  Created by Şahin Karahan on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct yugioh_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
